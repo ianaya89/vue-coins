b976edc3bc235c72fab5bd04ef9702282824c7ad
Ignacio Anaya <ignacio.anaya89@gmail.com>
2020-02-18 02:52:00 -0300

    Add workshop plugin

---
+++ src/components/CoinDetail.vue
@@ -0,0 +1,62 @@
+<template>
+  <section>
+  </section>
+</template>
+
+<script>
+export default {
+  name: 'CoinDetail',
+}
+</script>
+
+<style scoped>
+section {
+  position: fixed;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  width: 30%;
+  right: 5%;
+  border-radius: 1px;
+  border: 2px solid #78B28B;
+  padding: 1rem;
+  box-shadow: 15px 15px 15px -5px rgba(0,0,0,0.5);
+  animation: fadein .5s;
+}
+
+h3 {
+  display: flex;
+  width: 100%;
+  flex-direction: row;
+  justify-content: space-between;
+  margin: 1rem;
+}
+
+small {
+  cursor: pointer;
+  color: tomato;
+}
+
+ul {
+  padding: 0;
+  margin: 0;
+  list-style-type: none;
+}
+
+li {
+  margin-bottom: 1rem;
+}
+
+@keyframes fadein {
+  0% {
+    opacity: 0;
+    transform: translate3d(0, 20px, 0);
+  }
+
+  50%, 100% {
+    opacity: 1;
+    transform: translate3d(0, 0, 0);
+  }
+}
+</style>
---
+++ src/components/CoinList.vue
@@ -0,0 +1,89 @@
+<template>
+  <section tabindex="0">
+  </section>
+</template>
+
+<script>
+export default {
+  name: 'CoinList'
+}
+</script>
+
+<style scoped>
+section {
+  display: flex;
+}
+
+p {
+  text-transform: uppercase;
+  letter-spacing: 2px;
+  font-size: 2rem;
+  color: #3C495B;
+  font-weight: 600;
+  animation: rotate 2s;
+}
+
+table {
+  border-collapse: collapse;
+}
+
+th {
+  text-align: center;
+}
+
+td {
+  border-bottom: 2px solid #64B587;
+  margin: 0;
+}
+
+button:first-child:hover {
+  text-decoration: underline;
+}
+
+button {
+  background: transparent;
+  border: none;
+  cursor: pointer;
+  font-weight: 600;
+  color: #39495C;
+  font-size: 1rem;
+}
+
+td, th {
+  text-align: left;
+  min-width: 150px;
+  padding: 1rem;
+}
+
+input {
+  border: none;
+  font-size: 1rem;
+  padding: 1rem;
+}
+
+.selected::before {
+  content: "👉";
+}
+
+input::placeholder {
+  color: #000;
+  font-weight: 600;
+}
+
+@keyframes rotate {
+  0% {
+    transform: translate3d(0, 80px, 0);
+    opacity: 0;
+  }
+
+  20%, 80% {
+    transform: translate3d(0, 0, 0);
+    opacity: 1;
+  }
+
+  100% {
+    transform: translate3d(0, -40px, 0);
+    opacity: 0;
+  }
+}
+</style>
